2c70b14e82e55598562d3d9c83dace2972a02d11
Rochan543 <premrochan@gmail.com>
2026-01-06 20:58:34 +0530

    products-routes.js

--- server/routes/shop/products-routes.js
+++ server/routes/shop/products-routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const {
-  getFilteredProducts,
-  getProductDetails,
-} = require("../../controllers/shop/products-controller");
+  addProductReview,
+  getProductReviews,
+} = require("../../controllers/shop/product-review-controller");
 
 const router = express.Router();
 
-router.get("/get", getFilteredProducts);
-router.get("/get/:id", getProductDetails);
+router.post("/add", addProductReview);
+router.get("/:productId", getProductReviews);
 
 module.exports = router;
 
