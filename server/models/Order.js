ab30e252be0b2a9eef38d0e5b32535b900757968
Rochan543 <premrochan@gmail.com>
2026-01-06 20:48:14 +0530

    Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -75,4 +75,4 @@ const OrderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Order", OrderSchema);
+module.exports = mongoose.model("Order", OrderSchema)
